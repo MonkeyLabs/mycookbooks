45d39bd3fca6216e604e5675352864b14b01c393
Max Roman Bejarano <maxrb@choloclos.se>
2016-05-08 11:50:02 -0600

    Remove unrelevant recipes and add VPN recipe for SAP connection

---
+++ sap_vpn/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: sap_vpn
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cookbook_file "/etc/vpnc/SAP.conf" do
+  source "SAP.conf"
+  mode "0644"
+end
+
+cookbook_file "/etc/init/vpnc.conf" do
+  source "vpnc.conf"
+  mode "0644"
+end
+
+package vpnc
+
+package vpnc-scripts
+
+execute "sleep 5"
+
+service "vpnc" do
+  action :restart
+end
+
+ruby_block "insert_line_1" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/hosts")
+    file.insert_line_if_no_match("/192.168.200.20 cidrd00.alila.aliaxisla.com/", "192.168.200.20 cidrd00.alila.aliaxisla.com")
+    file.write_file
+  end
+end
+
+ruby_block "insert_line_2" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/hosts")
+    file.insert_line_if_no_match("/192.168.200.21 cidrq01.alila.aliaxisla.com/", "192.168.200.21 cidrq01.alila.aliaxisla.com")
+    file.write_file
+  end
+end

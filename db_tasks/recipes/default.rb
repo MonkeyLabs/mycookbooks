eec490604b402aecb560a260612e034f7ad9033b
Max Roman Bejarano <maxrb@choloclos.se>
2016-05-08 18:38:12 -0600

    Add hosts file information

--- db_tasks/recipes/default.rb
+++ db_tasks/recipes/default.rb
@@ -7,6 +7,22 @@
 # All rights reserved - Do Not Redistribute
 #
 
+ruby_block "insert_line_1" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/hosts")
+    file.insert_line_if_no_match("/192.168.200.20 cidrd00.alila.aliaxisla.com/", "192.168.200.20 cidrd00.alila.aliaxisla.com")
+    file.write_file
+  end
+end
+
+ruby_block "insert_line_2" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/hosts")
+    file.insert_line_if_no_match("/192.168.200.21 cidrq01.alila.aliaxisla.com/", "192.168.200.21 cidrq01.alila.aliaxisla.com")
+    file.write_file
+  end
+end
+
 bash 'run_migrate' do
   cwd '/srv/www/granbazar/current'
   code <<-EOH
@@ -23,3 +39,4 @@ bash 'run_seed' do
     echo "finish seed"
     EOH
 end
+

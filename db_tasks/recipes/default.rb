9f184f5e08d18c2ad89f766b9260b918be910d98
Max Roman Bejarano <maxrb@choloclos.se>
2016-05-08 23:11:15 -0600

    Add precompile on deploy taks withou mask

--- db_tasks/recipes/default.rb
+++ db_tasks/recipes/default.rb
@@ -7,27 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-ruby_block "insert_line_1" do
-  block do
-    file = Chef::Util::FileEdit.new("/etc/hosts")
-    file.insert_line_if_no_match("/192.168.200.20 cidrd00.alila.aliaxisla.com/", "192.168.200.20 cidrd00.alila.aliaxisla.com")
-    file.write_file
-  end
-end
-
-ruby_block "insert_line_2" do
-  block do
-    file = Chef::Util::FileEdit.new("/etc/hosts")
-    file.insert_line_if_no_match("/192.168.200.21 cidrq01.alila.aliaxisla.com/", "192.168.200.21 cidrq01.alila.aliaxisla.com")
-    file.write_file
-  end
-end
-
 bash 'run_migrate' do
   cwd '/srv/www/granbazar/current'
   code <<-EOH
     bundle exec rake db:migrate
-    echo finish
     EOH
 end
 
@@ -36,15 +19,13 @@ bash 'run_seed' do
   cwd '/srv/www/granbazar/current'
   code <<-EOH
     bundle exec rake db:seed
-    echo finish
     EOH
 end
 
-bash 'run_seed' do
+bash 'run_precompile' do
   cwd '/srv/www/granbazar/current'
   code <<-EOH
     bundle exec rake assets:precompile
-    echo finish
     EOH
 end
 

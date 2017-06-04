f077bb9536e441ee7fe880b0f5f11e499bc95836
Max Roman <maxrb@lemmy.choloclos.se>
2017-06-04 17:56:14 -0600

    Remove ftp and uploads and add path dependent

--- db_tasks/recipes/default.rb
+++ db_tasks/recipes/default.rb
@@ -8,7 +8,7 @@
 #
 
 bash 'run_migrate' do
-  cwd '/srv/www/granbazar/current'
+  cwd node[:dol_data][app][path]
   code <<-EOH
     bundle exec rake db:migrate;
     sleep 1;
@@ -17,7 +17,7 @@ end
 
 
 bash 'run_precompile' do
-  cwd '/srv/www/granbazar/current'
+  cwd node[:dol_data][app][path]
   code <<-EOH
     bundle exec rake assets:precompile;
     sleep 1;
@@ -25,7 +25,7 @@ bash 'run_precompile' do
 end
 
 bash 'run_whenever' do
-  cwd '/srv/www/granbazar/current'
+  cwd node[:dol_data][app][path]
   code <<-EOH
     crontab -r;
     bundle exec whenever -s 'environment=production' --update-crontab;
@@ -34,42 +34,15 @@ bash 'run_whenever' do
 end
 
 bash 'run_tmp_assets' do
-  cwd '/srv/www/granbazar/current/tmp'
+  cwd (node[:dol_data][app][path])+'/tmp'
   code <<-EOH
     chmod -R 777 .;
-    sleep 1;
-    EOH
-end
-
-bash 'run_link_uploads' do
-  cwd '/srv/www/granbazar/current/tmp'
-  code <<-EOH
-    mkdir -p /srv/www/granbazar/uploads;
-    rm -rd /srv/www/granbazar/current/public/uploads;
-    ln -s /srv/www/granbazar/uploads /srv/www/granbazar/current/public/uploads;
-    chmod -R 777 /srv/www/granbazar/uploads;
-    sleep 1;
-    EOH
-end
-
-bash 'run_public_uploads' do
-  cwd '/srv/www/granbazar/current/public/uploads'
-  code <<-EOH
-    chmod -R 777 .;
-    sleep 1;
-    EOH
-end
-
-bash 'run_link_ftp' do
-  cwd '/srv/www/granbazar/current/tmp'
-  code <<-EOH
-    ln -s /srv/uploads/ /srv/www/granbazar/current/public/account_status;
     sleep 1;
     EOH
 end
 
 bash 'run_seed' do
-  cwd '/srv/www/granbazar/current'
+  cwd node[:dol_data][app][path]
   code <<-EOH
     bundle exec rake db:seed;
     sleep 1;

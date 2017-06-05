159ca28fce8aa7deb3588ce5069e0c5c3373a574
Max Roman <maxrb@lemmy.choloclos.se>
2017-06-04 20:38:23 -0600

    NIcaragua

--- db_tasks/recipes/default.rb
+++ db_tasks/recipes/default.rb
@@ -48,3 +48,47 @@ bash 'run_seed' do
     sleep 1;
     EOH
 end
+
+#Nicaragua
+
+bash 'run_migrate' do
+  cwd '/srv/www/durmanonline_nicaragua/current'
+  code <<-EOH
+    bundle exec rake db:migrate;
+    sleep 1;
+    EOH
+end
+
+
+bash 'run_precompile' do
+  cwd '/srv/www/durmanonline_nicaragua/current'
+  code <<-EOH
+    bundle exec rake assets:precompile;
+    sleep 1;
+    EOH
+end
+
+bash 'run_whenever' do
+  cwd '/srv/www/durmanonline_nicaragua/current'
+  code <<-EOH
+    crontab -r;
+    bundle exec whenever -s 'environment=production' --update-crontab;
+    sleep 1;
+    EOH
+end
+
+bash 'run_tmp_assets' do
+  cwd '/srv/www/durmanonline_nicaragua/current/tmp'
+  code <<-EOH
+    chmod -R 777 .;
+    sleep 1;
+    EOH
+end
+
+bash 'run_seed' do
+  cwd '/srv/www/durmanonline_nicaragua/current'
+  code <<-EOH
+    bundle exec rake db:seed;
+    sleep 1;
+    EOH
+end
